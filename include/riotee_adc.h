0f5bfb01fdca55119e59b0dd78506928c60c0c35
Kai Geissdoerfer <kai.geissdoerfer@tu-dresden.de>
2023-06-12 21:47:28 +0200

    add batch sampling to ADC driver

--- include/riotee_adc.h
+++ include/riotee_adc.h
@@ -3,7 +3,98 @@
 
 #include <stdint.h>
 
-int riotee_adc_init();
-int riotee_adc_read(int16_t *dst, unsigned int analog_input);
+typedef enum {
+  RIOTEE_ADC_INPUT_NC = 0,
+  RIOTEE_ADC_INPUT_VCAP = 6,
+  RIOTEE_ADC_INPUT_A0 = 3,
+  RIOTEE_ADC_INPUT_A1 = 4,
+} riotee_adc_input_t;
+
+typedef enum {
+  RIOTEE_ADC_RESOLUTION_8BIT = 0UL,   ///< 8 bit resolution.
+  RIOTEE_ADC_RESOLUTION_10BIT = 1UL,  ///< 10 bit resolution.
+  RIOTEE_ADC_RESOLUTION_12BIT = 2UL,  ///< 12 bit resolution.
+  RIOTEE_ADC_RESOLUTION_14BIT = 3UL   ///< 14 bit resolution.
+} riotee_adc_resolution_t;
+
+typedef enum {
+  RIOTEE_ADC_OVERSAMPLE_DISABLED = 0UL,  ///< No oversampling.
+  RIOTEE_ADC_OVERSAMPLE_2X = 1UL,        ///< Oversample 2x.
+  RIOTEE_ADC_OVERSAMPLE_4X = 2UL,        ///< Oversample 4x.
+  RIOTEE_ADC_OVERSAMPLE_8X = 3UL,        ///< Oversample 8x.
+  RIOTEE_ADC_OVERSAMPLE_16X = 4UL,       ///< Oversample 16x.
+  RIOTEE_ADC_OVERSAMPLE_32X = 5UL,       ///< Oversample 32x.
+  RIOTEE_ADC_OVERSAMPLE_64X = 6UL,       ///< Oversample 64x.
+  RIOTEE_ADC_OVERSAMPLE_128X = 7UL,      ///< Oversample 128x.
+  RIOTEE_ADC_OVERSAMPLE_256X = 8UL       ///< Oversample 256x.
+} riotee_adc_oversample_t;
+
+typedef enum {
+  RIOTEE_ADC_GAIN1_6 = 0UL,  ///< Gain factor 1/6.
+  RIOTEE_ADC_GAIN1_5 = 1UL,  ///< Gain factor 1/5.
+  RIOTEE_ADC_GAIN1_4 = 2UL,  ///< Gain factor 1/4.
+  RIOTEE_ADC_GAIN1_3 = 3UL,  ///< Gain factor 1/3.
+  RIOTEE_ADC_GAIN1_2 = 4UL,  ///< Gain factor 1/2.
+  RIOTEE_ADC_GAIN1 = 5UL,    ///< Gain factor 1.
+  RIOTEE_ADC_GAIN2 = 6UL,    ///< Gain factor 2.
+  RIOTEE_ADC_GAIN4 = 7UL,    ///< Gain factor 4.
+} riotee_adc_gain_t;
+
+typedef enum {
+  RIOTEE_ADC_REFERENCE_INTERNAL = 0UL,  ///< Internal reference (0.6 V).
+  RIOTEE_ADC_REFERENCE_VDD4 = 1UL       ///< VDD/4 as reference.
+} riotee_adc_reference_t;
+
+typedef enum {
+  RIOTEE_ADC_ACQTIME_3US = 0UL,
+  RIOTEE_ADC_ACQTIME_5US = 1UL,
+  RIOTEE_ADC_ACQTIME_10US = 2UL,
+  RIOTEE_ADC_ACQTIME_15US = 3UL,
+  RIOTEE_ADC_ACQTIME_20US = 4UL,
+  RIOTEE_ADC_ACQTIME_40US = 5UL
+} riotee_adc_acqtime_t;
+
+typedef struct {
+  riotee_adc_gain_t gain;            ///< Gain control value.
+  riotee_adc_reference_t reference;  ///< Reference control value.
+  riotee_adc_acqtime_t acq_time;     ///< Acquisition time.
+  riotee_adc_input_t input_pos;
+  riotee_adc_input_t input_neg;
+  riotee_adc_oversample_t oversampling;
+  unsigned int n_samples;
+  unsigned int sample_interval_ticks32;
+} riotee_adc_cfg_t;
+
+int riotee_adc_init(void);
+
+/* Samples ADC into provided buffer. */
+int riotee_adc_sample(int16_t *dst, riotee_adc_cfg_t *cfg);
+
+/* Reads a single ADC sample. */
+static inline int riotee_adc_read(float *dst, riotee_adc_input_t input) {
+  int16_t adc_res;
+  int rc;
+  riotee_adc_cfg_t cfg = {.gain = RIOTEE_ADC_GAIN1_4,
+                          .reference = RIOTEE_ADC_REFERENCE_VDD4,
+                          .acq_time = RIOTEE_ADC_ACQTIME_5US,
+                          .input_pos = input,
+                          .input_neg = RIOTEE_ADC_INPUT_NC,
+                          .oversampling = RIOTEE_ADC_OVERSAMPLE_DISABLED,
+                          .n_samples = 1};
+
+  if ((rc = riotee_adc_sample(&adc_res, &cfg)) != 0)
+    return rc;
+  *dst = ((float)adc_res) / 4096.0f * 2.0f;
+  return 0;
+}
+
+/* Converts binary ADC result to voltage. */
+float riotee_adc_adc2vadc(int16_t adc, riotee_adc_cfg_t *cfg);
+
+/* Converts ADC voltage to capacitor voltage based on amplifier gain. */
+static inline float riotee_adc_vadc2vcap(float v_adc) {
+  /* A capacitor voltage of 4.8V produces 1.727V on the ADC input */
+  return v_adc / 1.727f * 4.8f;
+}
 
 #endif
--- src/adc.c
+++ src/adc.c
@@ -1,68 +1,125 @@
 #include "nrf.h"
-#include "nrf_saadc.h"
+#include "riotee_adc.h"
 #include "riotee.h"
 #include "FreeRTOS.h"
 #include "task.h"
 #include "runtime.h"
 
-/* This points to the task currently blocking on GPINT event */
-static TaskHandle_t waiting_task;
+#include "nrf_gpio.h"
 
-uint16_t adc_res;
+TEARDOWN_FUN(adc_teardown_ptr);
+
+/* Number of samples that still need to be taken before buffer is filled. */
+static unsigned int samples_remaining;
+
+/* Inverse gain lookup table, indexed by riotee_adc_gain_t */
+static const float gain_lut[] = {6.0f, 5.0f, 4.0f, 3.0f, 2.0f, 1.0f, 1.0f / 2, 1.0f / 4};
+/* Reference lookup table, indexed by enum riotee_adc_reference_t */
+static const float ref_lut[] = {0.6f, 0.5f};
+
+float riotee_adc_adc2vadc(int16_t adc, riotee_adc_cfg_t *cfg) {
+  float res_fac;
+  /* See nRF52833 Product Specification v1.5 sec 6.21.3*/
+  if (cfg->input_neg != RIOTEE_ADC_INPUT_NC) {
+    res_fac = 1.0f / (1 << 11);
+  } else {
+    res_fac = 1.0f / (1 << 12);
+  }
+  return ref_lut[cfg->reference] * ((float)adc) * gain_lut[cfg->gain] * res_fac;
+}
+
+static inline void stop_sampling(void) {
+  NRF_SAADC->INTENCLR = SAADC_INTENCLR_END_Msk;
+
+  NRF_RTC1->TASKS_STOP = 1;
+  NRF_SAADC->TASKS_STOP = 1;
+  NRF_SAADC->EVENTS_END = 0;
+
+  NRF_SAADC->ENABLE = (SAADC_ENABLE_ENABLE_Disabled << SAADC_ENABLE_ENABLE_Pos);
+  adc_teardown_ptr = NULL;
+}
 
 void SAADC_IRQHandler(void) {
   BaseType_t xHigherPriorityTaskWoken = pdFALSE;
 
-  if (NRF_SAADC->EVENTS_END == 1) {
-    NRF_SAADC->EVENTS_END = 0;
-    xTaskNotifyIndexedFromISR(waiting_task, 1, EVT_ADC, eSetValueWithOverwrite, &xHigherPriorityTaskWoken);
-  }
+  NRF_SAADC->EVENTS_END = 0;
 
-  portYIELD_FROM_ISR(xHigherPriorityTaskWoken);
+  if (--samples_remaining == 0) {
+    stop_sampling();
+    xTaskNotifyIndexedFromISR(usr_task_handle, 1, EVT_ADC, eSetBits, &xHigherPriorityTaskWoken);
+    portYIELD_FROM_ISR(xHigherPriorityTaskWoken);
+  } else {
+    NRF_SAADC->RESULT.PTR += 2;
+  }
 }
 
-int riotee_adc_init() {
-  NRF_SAADC->OVERSAMPLE = NRF_SAADC_OVERSAMPLE_DISABLED;
-  NRF_SAADC->RESOLUTION = NRF_SAADC_RESOLUTION_12BIT;
+static void teardown(void) {
+  stop_sampling();
+  xTaskNotifyIndexed(usr_task_handle, 1, EVT_TEARDOWN, eSetValueWithOverwrite);
+}
 
-  NRF_SAADC->CH[0].CONFIG =
-      ((NRF_SAADC_RESISTOR_DISABLED << SAADC_CH_CONFIG_RESP_Pos) & SAADC_CH_CONFIG_RESP_Msk) |
-      ((NRF_SAADC_RESISTOR_DISABLED << SAADC_CH_CONFIG_RESN_Pos) & SAADC_CH_CONFIG_RESN_Msk) |
-      ((NRF_SAADC_GAIN1_3 << SAADC_CH_CONFIG_GAIN_Pos) & SAADC_CH_CONFIG_GAIN_Msk) |
-      ((NRF_SAADC_REFERENCE_INTERNAL << SAADC_CH_CONFIG_REFSEL_Pos) & SAADC_CH_CONFIG_REFSEL_Msk) |
-      ((NRF_SAADC_ACQTIME_5US << SAADC_CH_CONFIG_TACQ_Pos) & SAADC_CH_CONFIG_TACQ_Msk) |
-      ((NRF_SAADC_MODE_SINGLE_ENDED << SAADC_CH_CONFIG_MODE_Pos) & SAADC_CH_CONFIG_MODE_Msk) |
-      ((NRF_SAADC_BURST_DISABLED << SAADC_CH_CONFIG_BURST_Pos) & SAADC_CH_CONFIG_BURST_Msk);
+int riotee_adc_init(void) {
+  NRF_SAADC->RESOLUTION = SAADC_RESOLUTION_VAL_12bit;
 
-  NRF_SAADC->RESULT.MAXCNT = 1;
-
-  NRF_SAADC->ENABLE = (SAADC_ENABLE_ENABLE_Enabled << SAADC_ENABLE_ENABLE_Pos);
+  NRF_PPI->CH[3].EEP = (uint32_t)&NRF_RTC1->EVENTS_TICK;
+  NRF_PPI->CH[3].TEP = (uint32_t)&NRF_SAADC->TASKS_START;
+  NRF_PPI->CHENSET = PPI_CHENSET_CH3_Msk;
 
   NRF_PPI->CH[4].EEP = (uint32_t)&NRF_SAADC->EVENTS_STARTED;
   NRF_PPI->CH[4].TEP = (uint32_t)&NRF_SAADC->TASKS_SAMPLE;
   NRF_PPI->CHENSET = PPI_CHENSET_CH4_Msk;
 
-  NRF_PPI->CH[5].EEP = (uint32_t)&NRF_SAADC->EVENTS_RESULTDONE;
+  NRF_PPI->CH[5].EEP = (uint32_t)&NRF_SAADC->EVENTS_END;
   NRF_PPI->CH[5].TEP = (uint32_t)&NRF_SAADC->TASKS_STOP;
   NRF_PPI->CHENSET = PPI_CHENSET_CH5_Msk;
 
-  NRF_SAADC->INTENSET = SAADC_INTENSET_END_Msk;
+  NRF_RTC1->EVTENSET = RTC_EVTENSET_TICK_Msk;
+
   NVIC_EnableIRQ(SAADC_IRQn);
 
   return 0;
 }
 
-int riotee_adc_read(int16_t *dst, unsigned int analog_input) {
+int riotee_adc_sample(int16_t *dst, riotee_adc_cfg_t *cfg) {
   unsigned long notification_value;
 
   taskENTER_CRITICAL();
-  waiting_task = xTaskGetCurrentTaskHandle();
-  /* PSELP value is AIN number plus one */
-  NRF_SAADC->CH[0].PSELP = analog_input + 1;
-  NRF_SAADC->RESULT.MAXCNT = 1;
+  NRF_SAADC->ENABLE = (SAADC_ENABLE_ENABLE_Enabled << SAADC_ENABLE_ENABLE_Pos);
+
+  NRF_SAADC->CH[0].CONFIG = (SAADC_CH_CONFIG_RESP_Bypass << SAADC_CH_CONFIG_RESP_Pos) |
+                            (SAADC_CH_CONFIG_RESN_Bypass << SAADC_CH_CONFIG_RESN_Pos) |
+                            ((cfg->gain << SAADC_CH_CONFIG_GAIN_Pos) & SAADC_CH_CONFIG_GAIN_Msk) |
+                            ((cfg->reference << SAADC_CH_CONFIG_REFSEL_Pos) & SAADC_CH_CONFIG_REFSEL_Msk) |
+                            ((cfg->acq_time << SAADC_CH_CONFIG_TACQ_Pos) & SAADC_CH_CONFIG_TACQ_Msk);
+
+  /* If oversampling is enabled, take samples as fast as possible in burst mode */
+  NRF_SAADC->OVERSAMPLE = cfg->oversampling;
+  if (cfg->oversampling != RIOTEE_ADC_OVERSAMPLE_DISABLED)
+    NRF_SAADC->CH[0].CONFIG |= (SAADC_CH_CONFIG_BURST_Enabled << SAADC_CH_CONFIG_BURST_Pos);
+
+  NRF_SAADC->CH[0].PSELP = cfg->input_pos;
+  NRF_SAADC->CH[0].PSELN = cfg->input_neg;
+  if (cfg->input_neg != RIOTEE_ADC_INPUT_NC)
+    NRF_SAADC->CH[0].CONFIG |= (SAADC_CH_CONFIG_MODE_Diff << SAADC_CH_CONFIG_MODE_Pos);
+
+  /* Take only one sample, manage buffer in software */
   NRF_SAADC->RESULT.PTR = (uint32_t)dst;
-  xTaskNotifyStateClearIndexed(waiting_task, 1);
-  NRF_SAADC->TASKS_START = 1;
+  NRF_SAADC->RESULT.MAXCNT = 1;
+  samples_remaining = cfg->n_samples;
+
+  xTaskNotifyStateClearIndexed(usr_task_handle, 1);
+
+  NRF_SAADC->INTENSET = SAADC_INTENSET_END_Msk;
+
+  /* Register teardown function so runtime can abort us */
+  adc_teardown_ptr = teardown;
+
+  if (cfg->n_samples > 1) {
+    NRF_RTC1->PRESCALER = cfg->sample_interval_ticks32 - 1;
+    NRF_RTC1->TASKS_START = 1;
+  } else
+    NRF_SAADC->TASKS_START = 1;
+
   taskEXIT_CRITICAL();
 
   xTaskNotifyWaitIndexed(1, 0xFFFFFFFF, 0xFFFFFFFF, &notification_value, portMAX_DELAY);
