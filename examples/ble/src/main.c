8d82137c4b2b771b7af186635d8966a379883e00
Kai Geissdoerfer <kai.geissdoerfer@tu-dresden.de>
2023-06-15 17:20:48 +0200

    add wait in ble example

--- examples/ble/src/main.c
+++ examples/ble/src/main.c
@@ -1,6 +1,7 @@
 
 #include "riotee.h"
 #include "riotee_ble.h"
+#include "riotee_timing.h"
 
 riotee_ble_ll_addr_t adv_address = {.addr_bytes = {0xBE, 0xEF, 0xDE, 0xAD, 0x00, 0x01}};
 
@@ -19,5 +20,7 @@ int main() {
     riotee_wait_cap_charged();
     riotee_ble_advertise(&ble_data, ADV_CH_ALL);
     ble_data.counter++;
+    /* Sleep at least 250ms before next advertising round */
+    riotee_sleep_ms(250);
   }
 }
