81cff698c3da9ec008bc4b95324c57b0cdbddd5b
Kai Geissdoerfer <kai.geissdoerfer@tu-dresden.de>
2023-06-16 12:38:04 +0200

    add uart example

---
+++ examples/uart/src/main.c
@@ -0,0 +1,19 @@
+
+#include "riotee.h"
+#include "riotee_timing.h"
+#include "riotee_uart.h"
+#include "printf.h"
+
+/* This gets called after every reset */
+void reset_callback(void) {
+  riotee_uart_init(PIN_D1, 1000000);
+  printf("Hello Riotee!\r\n");
+}
+
+int main(void) {
+  int counter = 0;
+  for (;;) {
+    printf("Loop: %d\r\n", counter++);
+    riotee_delay_ms(100);
+  }
+}
