8438e54903ae98c988826ca3e277c8efe0d6a41f
Kai Geissdoerfer <kai.geissdoerfer@tu-dresden.de>
2023-06-27 08:50:40 +0200

    gpio: add doxygen comments

--- core/include/riotee_gpio.h
+++ core/include/riotee_gpio.h
@@ -41,12 +41,23 @@ static inline nrf_gpio_port_t* riotee_gpio_get_port(unsigned int* pin) {
   return NRF_PORT0;
 }
 
+/**
+ * @brief Configures pin as output.
+ *
+ * @param pin Pin number.
+ */
 static inline void riotee_gpio_cfg_output(unsigned int pin) {
   nrf_gpio_port_t* reg = riotee_gpio_get_port(&pin);
 
   /* Output, input buffer disconnected */
   reg->PIN_CNF[pin] = (1UL << 0) | (1UL << 1);
 }
+
+/**
+ * @brief Configures pin as output.
+ *
+ * @param pin Pin number.
+ */
 static inline void riotee_gpio_cfg_input(unsigned int pin, riotee_gpio_in_pull_t pull) {
   nrf_gpio_port_t* reg = riotee_gpio_get_port(&pin);
 
@@ -54,24 +65,55 @@ static inline void riotee_gpio_cfg_input(unsigned int pin, riotee_gpio_in_pull_t
   reg->PIN_CNF[pin] = (pull << 2);
 }
 
+/**
+ * @brief Sets output pin to logic high.
+ *
+ * @param pin Pin number.
+ */
 static inline void riotee_gpio_set(unsigned int pin) {
   nrf_gpio_port_t* reg = riotee_gpio_get_port(&pin);
   reg->OUTSET = (1UL << pin);
 }
+
+/**
+ * @brief Sets output pin to logic low.
+ *
+ * @param pin Pin number.
+ */
 static inline void riotee_gpio_clear(unsigned int pin) {
   nrf_gpio_port_t* reg = riotee_gpio_get_port(&pin);
 
   reg->OUTCLR = (1UL << pin);
 }
+
+/**
+ * @brief Toggles logic state of output pin.
+ *
+ * @param pin Pin number.
+ */
 static inline void riotee_gpio_toggle(unsigned int pin) {
   nrf_gpio_port_t* reg = riotee_gpio_get_port(&pin);
 
   reg->OUT ^= (1UL << pin);
 }
+
+/**
+ * @brief Reads logic state of input pin.
+ *
+ * @param pin Pin number.
+ * @return uint32_t 1 if logic high, 0 if logic low
+ */
 static inline uint32_t riotee_gpio_read(unsigned int pin) {
   nrf_gpio_port_t* reg = riotee_gpio_get_port(&pin);
   return (reg->IN >> pin) & 1UL;
 }
+
+/**
+ * @brief Reads logic state of output pin.
+ *
+ * @param pin Pin number.
+ * @return uint32_t 1 if logic high, 0 if logic low
+ */
 static inline uint32_t riotee_gpio_is_set(unsigned int pin) {
   nrf_gpio_port_t* reg = riotee_gpio_get_port(&pin);
   return (reg->OUT >> pin) & 1UL;
