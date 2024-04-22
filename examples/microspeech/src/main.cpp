142e47fe5d64d5a67bbdd45a2471d150f6a1e9db
Kai Geissdoerfer <kai.geissdoerfer@tu-dresden.de>
2024-04-22 15:58:11 -0600

    microspeech: change to battery-free mode

--- examples/microspeech/src/main.cpp
+++ examples/microspeech/src/main.cpp
@@ -1,3 +1,5 @@
+#include "string.h"
+
 #include "tensorflow/lite/core/c/common.h"
 #include "micro_model_settings.h"
 
@@ -6,28 +8,39 @@
 #include "riotee_adc.h"
 #include "riotee_gpio.h"
 #include "riotee_timing.h"
+#include "riotee_stella.h"
+#include "riotee_thresholds.h"
 
-#include "string.h"
 #include "vm1010.h"
+#include "shtc3.h"
 
 /* Pin D10 enables/disables microphone on the Riotee Sensor Shield (low active)
  */
 #define PIN_MICROPHONE_DISABLE PIN_D5
 #define N_SAMPLES 16000
 
-/* This gets called after every reset */
-void reset_callback(void) {
-  riotee_gpio_cfg_output(PIN_LED_CTRL);
-  riotee_gpio_cfg_output(PIN_D7);
+void startup_callback(void) {
+  /* Call this early to put SHTC3 into low power mode */
+  shtc3_init();
+  /* Disable microphone to reduce current consumption. */
   riotee_gpio_cfg_output(PIN_MICROPHONE_DISABLE);
+  riotee_gpio_set(PIN_MICROPHONE_DISABLE);
+}
+
+void reset_callback(void) {
+  riotee_thresholds_low_set(THR_LOW_3V1);
 
   vm1010_cfg_t cfg = {.pin_mode = PIN_D10, .pin_dout = PIN_D4, .pin_vout = PIN_D2, .pin_vbias = PIN_D3};
   vm1010_init(&cfg);
-  /* Switch on microphone */
-  riotee_gpio_clear(PIN_MICROPHONE_DISABLE);
 }
 
-int16_t samples[16000];
+void suspend_callback(void) {
+  /* Disable the microphone */
+  riotee_gpio_set(PIN_MICROPHONE_DISABLE);
+  vm1010_exit();
+}
+
+int16_t samples[N_SAMPLES];
 
 /* Remove mean and scale audio to fit int16 range */
 void prescale_audio(int16_t *dst, const size_t n_samples) {
@@ -60,22 +73,35 @@ void prescale_audio(int16_t *dst, const size_t n_samples) {
 int main(void) {
   int rc;
   struct ClassificationResult result;
+  printf("main..\r\n");
 
   for (;;) {
-    riotee_gpio_set(PIN_LED_CTRL);
-    riotee_sleep_ms(100);
-    riotee_gpio_clear(PIN_LED_CTRL);
+    riotee_wait_cap_charged();
+
+    /* Enable microphone */
+    riotee_gpio_clear(PIN_MICROPHONE_DISABLE);
+    /* Wait for 2ms for V_BIAS to come up */
+    riotee_sleep_ticks(70);
+    printf("Activating wake-on-sound..\r\n");
 
     /* Wait for wake-on-sound signal from microphone */
     if ((rc = vm1010_wait4sound()) != RIOTEE_SUCCESS) {
       printf("Error while waiting for sound: %d", rc);
     }
-    /* Wait until microphone can be sampled (see VM1010 datasheet)*/
+    /* Wait until microphone can be sampled.. */
     riotee_sleep_ms(2);
 
     /* Sample with 16384Hz instead of the 16000 expected by the model */
     rc = vm1010_sample(samples, N_SAMPLES, 2);
+    /* Disable the microphone */
+    riotee_gpio_set(PIN_MICROPHONE_DISABLE);
 
+    if (rc != RIOTEE_SUCCESS) {
+      printf("Sampling failed with %d\r\n", rc);
+      continue;
+    }
+
+    printf("Sampling done. Start processing..\r\n");
     prescale_audio(samples, N_SAMPLES);
 
     if ((rc = Classify(result, samples, N_SAMPLES)) == kTfLiteOk)
