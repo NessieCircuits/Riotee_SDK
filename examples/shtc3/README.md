4ca79c1f23ce71ca270a27030881ddd71b70f169
Kai Geissdoerfer <kai.geissdoerfer@tu-dresden.de>
2024-05-11 10:46:17 -0600

    add example for shtc3 temperature and humidity sensor

---
+++ examples/shtc3/README.md
@@ -0,0 +1,3 @@
+# SHTC3 Example
+
+Uses the SHTC3 temperature and humidity sensor onboard the Riotee Sensor shield to measure temperature and humidity and send the result over BLE.
---
+++ examples/shtc3/src/main.c
@@ -0,0 +1,40 @@
+
+#include "riotee.h"
+#include "riotee_gpio.h"
+#include "riotee_timing.h"
+#include "riotee_ble.h"
+
+#include "shtc3.h"
+#include "printf.h"
+
+const uint8_t adv_address[] = {0x01, 0xEE, 0xC0, 0xFF, 0x03, 0x02};
+const char adv_name[] = "RIOTEE";
+
+static shtc3_res_t th_result;
+
+void startup_callback(void) {
+  /* Call this early to put SHTC3 into low power mode */
+  shtc3_init();
+}
+
+void reset_callback(void) {
+  riotee_ble_init();
+}
+
+int main(void) {
+  riotee_ble_adv_cfg_t adv_cfg = {.addr = adv_address,
+                                  .name = adv_name,
+                                  .name_len = 6,
+                                  .data = &th_result,
+                                  .data_len = sizeof(shtc3_res_t),
+                                  .manufacturer_id = RIOTEE_BLE_ADV_MNF_NORDIC};
+  riotee_ble_adv_cfg(&adv_cfg);
+
+  for (;;) {
+    riotee_wait_cap_charged();
+    shtc3_read(&th_result);
+    riotee_ble_advertise(ADV_CH_ALL);
+    printf("Temperature: %.2f°C, Humidity: %.2f%%\r\n", th_result.temp, th_result.humidity);
+    riotee_sleep_ms(500);
+  }
+}
