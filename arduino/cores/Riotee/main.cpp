a394d6e3f5a7fa58d499cf83d7401408fa03358c
Kai Geissdoerfer <kai.geissdoerfer@tu-dresden.de>
2025-06-03 19:33:25 +0200

    arduino: remove redundant callback calls

--- arduino/cores/Riotee/main.cpp
+++ arduino/cores/Riotee/main.cpp
@@ -3,22 +3,6 @@
 
 #include "external/ArduinoCore-API/api/Common.h"
 
-void startup(void);
-void bootstrap(void);
-
-__attribute__((weak)) void startup(void){};
-__attribute__((weak)) void bootstrap(void){};
-
-/* This gets called very early, before setting up memory */
-void earlyinit(void) {
-  startup();
-}
-
-/* This gets called one time after flashing new firmware */
-void bootstrap(void) {
-  bootstrap();
-}
-
 /* This gets called after every reset */
 void lateinit(void) {
   riotee_adc_init();
