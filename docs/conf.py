14a18ab816b106b9c42e6a21dd9e1156ed3008bb
Kai Geissdoerfer <kai.geissdoerfer@tu-dresden.de>
2025-06-08 16:20:02 +0200

    Bump version: 1.1.1 → 1.2.0

--- docs/conf.py
+++ docs/conf.py
@@ -9,7 +9,7 @@
 project = "Riotee docs"
 copyright = "2023, Kai Geissdoerfer"
 author = "Kai Geissdoerfer"
-release = "1.1.1"
+release = "1.2.0"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
